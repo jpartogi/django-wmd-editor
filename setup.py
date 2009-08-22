f1f7c41b6e3adedc0b5b3ba4de4e91d3ff2bd747
Joshua Partogi <jpartogi@scrum8.com>
2009-08-22 23:00:06 +1000

    added distutils setup

--- setup.py
+++ setup.py
@@ -1,1 +1,31 @@
-# $Id: setup.py 6d33f21c6e8a 2009/08/15 06:33:15 jpartogi $
+# $Id: setup.py d4ce2551f91d 2009/08/22 13:00:06 jpartogi $
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+app_name = 'wmd'
+
+setup(name='django-%s-editor' % app_name,
+        version='0.9.0',
+        packages=[app_name, '%s.templatetags' % app_name],
+        package_data = {app_name: ['templates/wmd.*', 'templates/wmd/*.js', 'templates/wmd/images/*.png']},
+        author = 'Joshua Partogi',
+        author_email = 'joshua.partogi@gmail.com',
+        url = 'http://bitbucket.org/scrum8/django-wmd-editor/',
+        download_url = 'http://bitbucket.org/scrum8/django-wmd-editor/downloads/',
+
+        license = "BSD License",
+        keywords = "django wmd editor",
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Application Frameworks',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        platforms = ['any'],
+)
