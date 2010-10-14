a6c0ce305f53acf3a851a436cf1fb303f6aa5656
Aaron Madison <aaron.madison@theimtgroup.com>
2010-10-14 16:28:09 -0500

    removed unnecessary items

--- wmd/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^wmd/css/$', 'wmd.views.wmd_css', name='wmd-css'),
-)
--- wmd/views.py
+++ wmd/views.py
@@ -1,16 +1,1 @@
-from django.core.cache import cache
-from django.http import HttpResponse
-from django.template import loader, RequestContext
-
-from wmd import settings as wmd_settings
-from wmd.models import *
-
-def wmd_css(request):
-    template_files = (
-        'wmd.css',
-    )
-    template = loader.select_template(template_files)
-
-    context = RequestContext(request)
-    
-    return HttpResponse(template.render(context), content_type="text/css")
+# put your views here
