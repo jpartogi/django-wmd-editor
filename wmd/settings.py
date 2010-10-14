56b24ce48a1857f1b3c9a5b466c84da3f1676dcc
Aaron Madison <aaron.l.madison@gmail.com>
2010-10-13 19:45:13 -0500

    removed a bunch of unnecessary stuff

--- wmd/settings.py
+++ wmd/settings.py
@@ -1,9 +1,5 @@
+
 from django.conf import settings
 
-
-WMD_OUTPUT = getattr(settings, 'WMD_OUTPUT', "Markdown")
-WMD_LINE_LENGTH = getattr(settings, 'WMD_LINE_LENGTH', 40)
-WMD_BUTTONS = getattr(settings, 'WMD_BUTTONS', "bold italic | link blockquote code image | ol ul heading hr")
-
 WMD_SHOW_PREVIEW = getattr(settings, 'WMD_SHOW_PREVIEW', False)
-WMD_ADMIN_SHOW_PREVIEW = getattr(settings, 'WMD_ADMIN_SHOW_PREVIEW', False)
+WMD_ADMIN_SHOW_PREVIEW = getattr(settings, 'WMD_ADMIN_SHOW_PREVIEW', True)
--- wmd/templates/wmd-settings.js
+++
@@ -1,5 +0,0 @@
-wmd_options = {
-    output: "{{ WMD_OUTPUT }}",
-    lineLength: {{WMD_LINE_LENGTH}},
-    buttons: "{{ WMD_BUTTONS }}"
-};
